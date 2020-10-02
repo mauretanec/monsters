9429ba29f15744cebba9d8135e9026cb34038af9
Jan Bulava <jan.bulava@tietoevry.com>
2020-10-02 21:31:21 +0200

    Add files for github pages

---
+++ src/components/card/card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.styles.css'
+
+export const Card = ({ id, name, email }) => {
+  return (
+    <div className='card'>
+      <img alt="monster" src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+      <h2> {name} </h2>
+      <p> {email} </p>
+    </div>
+  )
+}
