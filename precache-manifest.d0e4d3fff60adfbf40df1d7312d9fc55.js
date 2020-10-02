4dc17057b2176971da7d783bb80a03a66fc45018
Jan Bulava <jan.bulava@tietoevry.com>
2020-10-02 21:27:01 +0200

    Updates

---
+++ precache-manifest.d0e4d3fff60adfbf40df1d7312d9fc55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b511b4351b6b989e1ad2aa602455b4e2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "94ef80fd777ce12602aa",
+    "url": "/monsters/static/css/main.9a25f50d.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ef80fd777ce12602aa",
+    "url": "/monsters/static/js/main.d6469ffc.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
